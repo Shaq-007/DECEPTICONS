792838b365b5f4948abaedef5f983df135a06e3b
GitHub User Name <email>
2021-01-12 16:13:55 -0700

    MR - css updated for cardboard

--- client/src/fakeDatabase.js/funWords.js
+++ client/src/fakeDatabase.js/funWords.js
@@ -24,7 +24,7 @@ export const funWords = [
         back: "Messele",
     }, 
     {
-        front: "EvolveU",
-        back: "InceptionU",
+        front: "Illyas",
+        back: "Jason",
     }
 ]
--- client/src/pages/PlayPage.js
+++ client/src/pages/PlayPage.js
@@ -7,11 +7,11 @@ const PlayPage = () => {
   return (
     <div>
       <div className="container containerAlignment">
-        {/* <div className="row align-items-center rowAlignment">
-          <div className="col"> */}
+        <div className="row align-items-center rowAlignment">
+          <div className="col">
             <CardBoard funWords={ funWords }/>
-          {/* </div>
-       </div> */}
+          </div>
+       </div>
     </div>
   </div>
   );
