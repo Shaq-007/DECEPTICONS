2a974db872d1c8b595eb591e1284d41ab6d59e8b
AgataPalka <agata.palka27@gmail.com>
2021-01-13 12:19:55 -0700

    adding css for layout

--- client/src/fakeDatabase.js/funWords.js
+++ client/src/fakeDatabase.js/funWords.js
@@ -1,30 +1,50 @@
 export const funWords = [
-    {
-        front: "Al",
-        back: "Tony",
-    }, 
-    {
-        front: "Mona",
-        back: "Agata",
-    }, 
-    {
-        front: "Shakeel",
-        back: "Liliana",
-    }, 
-    {
-        front: "Jen",
-        back: "Margo",
-    }, 
-    {
-        front: "Chris",
-        back: "Greg",
-    }, 
-    {
-        front: "Danielle",
-        back: "Messele",
-    }, 
-    {
-        front: "Illyas",
-        back: "Jason",
-    }
-]
+  {
+    front: "Al",
+    back: "Tony",
+  },
+  {
+    front: "Mona",
+    back: "Agata",
+  },
+  {
+    front: "Shakeel",
+    back: "Liliana",
+  },
+  {
+    front: "Jen",
+    back: "Margo",
+  },
+  {
+    front: "Chris",
+    back: "Greg",
+  },
+  {
+    front: "Danielle",
+    back: "Messele",
+  },
+  {
+    front: "Iliyas",
+    back: "Jason",
+  },
+  {
+    front: "Amenze",
+    back: "Kurt",
+  },
+  {
+    front: "Fernando",
+    back: "Jason",
+  },
+  {
+    front: "Sam",
+    back: "Jill",
+  },
+  {
+    front: "Bret",
+    back: "Sid",
+  },
+  {
+    front: "Henry",
+    back: "Jason",
+  },
+];
