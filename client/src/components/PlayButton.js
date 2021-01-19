9e168ee643dfe86f88a1272c15cb7dbc39fc81a8
AgataPalka <agata.palka27@gmail.com>
2021-01-19 11:29:30 -0700

    AP - creating Play button

---
+++ client/src/components/PlayButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PlayButton = () => {
+  return (
+    <div>
+      <a href="./categories">
+        <img
+          src="https://img.icons8.com/bubbles/2x/play.png"
+          alt="play button"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default PlayButton;
