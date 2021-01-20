bd4c3f346409d77dda978a19bca9030488d6b202
GitHub User Name <email>
2021-01-19 19:49:53 -0700

    MR - categories page now responsive

--- client/src/pages/CategoriesPage.js
+++ client/src/pages/CategoriesPage.js
@@ -1,22 +1,17 @@
 import React from "react";
 import CategoryButtons from "../components/CategoryButtons";
 import "../components/CategoriesButtons.css";
-import background2 from "../images/background2.jpg";
 
 const CategoriesPage = () => {
   return (
-    <div className="img-fluid bgImage">
-      <div className="container buttonsAlignment">
-        <div className="row">
-          <div className="col">
-            <CategoryButtons value='Animals' styleClass="btn-outline-secondary btn-lg btn-block button-image animals"/>
-            <CategoryButtons value='Shapes' styleClass="btn-outline-secondary btn-lg btn-block button-image shapes"/>
-            <CategoryButtons value='Letters' styleClass="btn-outline-secondary btn-lg btn-block button-image letters"/>
-            <CategoryButtons value='Custom' styleClass="btn-outline-secondary btn-lg btn-block button-image custom"/>
-          </div>
-        </div>
+    <div className="container bgImage">
+      <div className="buttonsContainer">
+          <CategoryButtons value='Animals' styleClass="btn-outline-secondary btn-block buttonsAlignment button-image animals" />
+          <CategoryButtons value='Shapes' styleClass="btn-outline-secondary btn-block buttonsAlignment button-image shapes" />
+          <CategoryButtons value='Letters' styleClass="btn-outline-secondary btn-block buttonsAlignment button-image letters" />
+          <CategoryButtons value='Custom' styleClass="btn-outline-secondary btn-block buttonsAlignment button-image custom" />
       </div>
-  </div>
+    </div>
   )
 };
 
