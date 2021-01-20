7a563a44f1ef03ba0eab5a4a8a7e372e1da97c1d
Liliana Zuleta <zliliana@hotmailcom>
2021-01-20 14:33:45 -0700

    sending test page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Categories from "./pages/CategoriesPage";
 import PlayPage from "./pages/PlayPage";
+import TestPage from "./pages/TestPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/categories" component={Categories} />
         <Route exact path="/play" component={PlayPage}/>
+        <Route exact path="/test" component={TestPage}/>
       </Switch>
     </Router>
   );
