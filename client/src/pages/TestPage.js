08fe10e7fab18a1b5e272c4b5dc5f6bd35012ef5
Liliana Zuleta <zliliana@hotmailcom>
2021-01-20 17:03:45 -0700

    Added images call in react

--- client/src/pages/TestPage.js
+++ client/src/pages/TestPage.js
@@ -1,23 +1,53 @@
 import React, { useState, useEffect } from "react";
 
-const CategoriesPage = () => {
-  const [users, setUsers] = useState([{}]);
+const TestPage = () => {
+  const [images, setImages] = useState([{}]);
 
   // useEffect(() => {
-  const getUsers = async () => {
-    let response = await fetch("/api/users");
+  const getImages = async () => {
+    let response = await fetch("/api/images");
     let data = await response.json();
-    setUsers(data);
+    setImages(data);
   };
   // }, []);
-  console.log("these are the users :", users);
+  console.log("these are the images :", images);
   return (
     <div>
-      <div>Categories Page</div>
-      <button onClick={getUsers}>Click me</button>
-      <div>{users[0].username}</div>
+      <div>Test Page</div>
+      <button onClick={getImages}>Click me</button>
+      <div>{images[0].name}</div>
+      <div>{images[0].categoryName}</div>
+      <div>{images[0].img.data}</div>
+      
+      {/* <img src={`data:image/jpeg;base64,${images[0].img.data}`} /> */}
     </div>
   );
 };
 
-export default CategoriesPage;
+export default TestPage;
+
+
+
+// import React, { useState, useEffect } from "react";
+
+// const CategoriesPage = () => {
+//   const [users, setUsers] = useState([{}]);
+
+//   // useEffect(() => {
+//   const getUsers = async () => {
+//     let response = await fetch("/api/users");
+//     let data = await response.json();
+//     setUsers(data);
+//   };
+//   // }, []);
+//   console.log("these are the users :", users);
+//   return (
+//     <div>
+//       <div>Categories Page</div>
+//       <button onClick={getUsers}>Click me</button>
+//       <div>{users[0].username}</div>
+//     </div>
+//   );
+// };
+
+// export default CategoriesPage;
