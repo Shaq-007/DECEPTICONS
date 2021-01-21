a57b5bd8bb737aa743e6a2e34b1ac8af471f9771
AgataPalka <agata.palka27@gmail.com>
2021-01-20 17:10:13 -0700

    AP - changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Categories from "./pages/CategoriesPage";
 import PlayPage from "./pages/PlayPage";
-<<<<<<< HEAD
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
-=======
 import TestPage from "./pages/TestPage";
->>>>>>> 12fd3ad0f54d5e0b6e0ab4a086b0ba1ea05894c9
 
 function App() {
   return (
@@ -17,13 +14,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/categories" component={Categories} />
-        <Route exact path="/play" component={PlayPage}/>
-<<<<<<< HEAD
-        <Route exact path="/home" component={HomePage}/>
-        <Route exact path="/signup" component={SignupPage}/>
-=======
-        <Route exact path="/test" component={TestPage}/>
->>>>>>> 12fd3ad0f54d5e0b6e0ab4a086b0ba1ea05894c9
+        <Route exact path="/play" component={PlayPage} />
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/signup" component={SignupPage} />
+        <Route exact path="/test" component={TestPage} />
       </Switch>
     </Router>
   );
