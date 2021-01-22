b75ce43091f831a6f78b08b855ba5b8a88852bdb
Liliana Zuleta <zliliana@hotmailcom>
2021-01-22 09:08:10 -0700

    CRUD fixes

--- server/controllers.js
+++ server/controllers.js
@@ -1,28 +1,122 @@
-const express = require('express');
-// const mongoose = require("mongoose");
-const { User, Image } = require('./db/models');
+const express = require("express");
+const { User, Image } = require("./db/models");
 
-// const app = express();
 //Set up mongoose connection
 require("./db/mongoose");
 
-// app.get('/',(req,res) => {
-//     res.send('Hello world!!!');
-// });
+// add new user to database
+exports.create_user = async function (req, res) {
+  const newUser = req.body;
+  // console.log(req.body)
+  const newRecord = {
+    username: newUser.username,
+    email: newUser.email,
+    password: newUser.password,
+    userlevel: newUser.userlevel,
+  };
+  try {
+    let response = await User.create(newRecord);
+    console.log("User created OK");
+    res.status(200).json(response.toJSON());
+  } catch (error) {
+    // failure
+    console.log(`Error: user error: ${error}`);
+    if (error.name === "MongoError" && error.code === 11000) {
+      // duplicate user and/or email
+      console.log("Username or email already used");
+      let data = {
+        status: 500,
+        message: "Username or email already used",
+      };
+      res.status(500).send(data);
+    } else {
+      console.log("internal server error");
+      res.status(500).json(error);
+    }
+  }
+  return newRecord;
+};
 
+exports.get_users = async function () {
+  const users = await User.find();
+  return users;
+};
 
-exports.get_user = async function () {
-    const users = await User.find();
-    return users
+// Display detail  for ONE user.
+exports.get_oneuser = async function (req,res) {
+    console.info('in controller:', req.params.userid)
+    try{
+        const oneuser = await User.findById(req.params.userid);
+        if (!oneuser) {
+            return res.status(404).send()
+        }
+        res.send(oneuser)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+    return 
+};
+
+//// To update a document changing userlevel to 4
+exports.update_userLevel = async function (req, res) {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+  return user;
+};
+
+/// Remove a user 
+exports.delete_user = async function (req, res) {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+           return res.status(404).send() 
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
 }
 
-exports.get_image_category = async function () {
-    const imagesByCategory = await Image
-    .find({categoryName:'Colors'});
-    console.log(imagesByCategory)
-    return imagesByCategory
-} 
 
+
+
+
+
+
+
+/// Remove a document from Project2
+// exports.delete_user = async function (req, res) {
+//   const user = req.body.username;
+
+//   // delete a single entry 
+//   const response = await User.deleteOne({ username: user });
+//   const result = JSON.stringify(response);
+
+//   if (response.deletedCount != 0) {
+//     console.log(`Success: user deleted: ${user}`); // success
+//     console.log(`Delete user response: ${result}`);
+//     res.status(200).json(response);
+//   } else {
+//     console.log(`Error: unable to delete user: ${result}`); // failure
+//     res.status(500).json(response);
+//   }
+// };
+
+exports.get_image_category = async function () {
+  const imagesByCategory = await Image.find({ categoryName: "Colors" });
+  console.log(imagesByCategory);
+  return imagesByCategory;
+};
 
 // app.get('/api/users/:id', async (req,res,next) => {
 //     let userid = req.params.id
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 // const mongoose = require("mongoose");
 // const { User } = require('./db/models');
-let controllers = require('./controllers')
+let controllers = require("./controllers");
+const { User } = require("./db/models");
 const app = express();
 //Set up mongoose connection
 // const app = require('./app');
@@ -10,28 +11,73 @@ const app = express();
 // app.get('/',(req,res) => {
 //     res.send('Hello world!!!');
 // });
+app.use(express.json());
 
-
-app.get('/api/users', async (req,res) => {
-    const users = await controllers.get_user()
-    if (users != null) {
-        console.log(`success, users found: ${users}`);
-        res.status(200).json(users);
-    } else {
-        console.log('error:cannot find users');
-        res.sendStatus(400)
-    }
+/// api call to create new users
+app.post("/api/create", async (req, res) => {
+  console.log(req.body);
+  const newUser = await controllers.create_user(req, res);
 });
 
-app.get('/api/images', async (req,res) =>{
-    const images = await controllers.get_image_category()
-    if (images != null) {
-        console.log(`success, images found: ${images}`);
-        res.status(200).json(images);
-    } else {
-        console.log('error:cannot find images');
-        res.sendStatus(400)
-    }
+// app.post('/api/create',(req, res) => {
+//     console.log(req.body)
+//     res.send('testing')
+// })
+
+/// api call to get all users
+app.get("/api/users", async (req, res) => {
+  const users = await controllers.get_users();
+  if (users != null) {
+    // console.log(`success, users found: ${users}`);
+    res.status(200).json(users);
+  } else {
+    console.log("error:cannot find users");
+    res.sendStatus(400);
+  }
+});
+
+app.get("/api/:userid", async (req, res) => {
+  console.info("we are looking for user:", req.params.userid);
+  let data = await controllers.get_oneuser(req, res);
+  res.send(data);
+});
+
+//////// this get works but it is not separated 
+// app.get("/api/:userid", async (req, res) => {
+//     const _id = req.params.userid
+//     User.findById(_id).then((user) => {
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     }).catch ((e) => {
+//         res.status(500).send()
+//     })
+// })
+
+
+
+
+
+app.patch("/api/update/:id", async (req, res) => {
+  console.log(req.body);
+  const updateuser = await controllers.update_userLevel(req, res);
+  res.send(updateuser);
+});
+
+app.delete("/api/delete/:id", async (req,res) => {
+  await controllers.delete_user(req,res);
+});
+
+app.get("/api/images", async (req, res) => {
+  const images = await controllers.get_image_category();
+  if (images != null) {
+    console.log(`success, images found: ${images}`);
+    res.status(200).json(images);
+  } else {
+    console.log("error:cannot find images");
+    res.sendStatus(400);
+  }
 });
 
 // app.get('/api/users/:id', async (req,res,next) => {
