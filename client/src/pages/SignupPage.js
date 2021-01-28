3e619c0d10b4406c5fb2b346c4e7ace4548e7e9b
AgataPalka <agata.palka27@gmail.com>
2021-01-28 09:02:13 -0700

    AP - adding mobile responsiveness to SignupPage

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -1,34 +1,47 @@
 import React from "react";
-import '../components/Signuppage.css';
-import '../components/Fonts.css';
+import "../components/SignupPage.css";
+import "../components/Fonts.css";
 const SignupPage = () => {
   return (
-   
-    <div className="container Background">
-      <h1 className="title">Memory Land</h1>
-
-       <div className="formAlignment">
-           
-             <div className="form-text instruction">New User Signup</div>
-             <div><label for="exampleInputName" className="form-label">Name</label>
-             <input type="name" className="form-control" id="exampleInputName" aria-describedby="namelHelp" />
-            <label for="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
-            <label for="exampleInputPassword1" className="form-label">Confirm Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
-            </div>
-            <div class="mb-3"></div>
-               
-                <br/><button type="submit" className="btn btn-warning">Sign Up</button>
+    <div className="BackgroundSignUpImage">
+      <div className="formAlignmentSignUp">
+        <div className="form-text instructionSignUp">New User Signup</div>
+        <div>
+          <label for="exampleInputName" className="form-label">
+            Name
+          </label>
+          <input
+            type="name"
+            className="form-control"
+            id="exampleInputName"
+            aria-describedby="namelHelp"
+          />
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+          <label for="exampleInputPassword1" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword2"
+          />
         </div>
-        
+        <div className="mb-3"></div>
+
+        <br />
+        <button type="submit" className="btn btn-warning">
+          Sign Up
+        </button>
       </div>
-   
-
-
-    
-   
-  )
+    </div>
+  );
 };
 
 export default SignupPage;
