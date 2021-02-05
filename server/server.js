2a12b48899d86d7174fe0c5937959f7b68cd21bf
Liliana Zuleta <zliliana@hotmailcom>
2021-02-04 18:08:05 -0700

    Cleanin up code - deleting commented code

--- server/server.js
+++ server/server.js
@@ -3,21 +3,14 @@ let upload_controller = require("./controllers/imageupload");
 let user_controllers = require("./controllers/users");
 let image_controllers = require("./controllers/images");
 const app = express();
-const authRoutes = require('./routes/auth'); //from auth
+const authRoutes = require("./routes/auth"); //from auth
 app.use(express.json());
 
-
-// app.get('/',(req,res) => {
-//     res.send('Hello world!!!');
-// });
-
-
 //from Auth APIs
-
-app.use('/api', authRoutes);
+app.use("/api", authRoutes);
 
 /// api call to create new users
-app.post("/users/create", user_controllers.create_user);
+// app.post("/users/create", user_controllers.create_user);
 
 app.get("/users", user_controllers.get_users);
 
@@ -29,12 +22,6 @@ app.delete("/users/delete/:userid", user_controllers.delete_user);
 
 // api GET calls for images depending on CATEGORY
 app.get("/images/colors", image_controllers.get_image_colors);
-
-// app.get("/images/animals", image_controllers.get_image_animals);
-
-// app.get("/images/shapes", image_controllers.get_image_shapes);
-
-// app.get("/images/letters", image_controllers.get_image_letters);
 
 app.get("/images/:categoryName", image_controllers.get_image_custom);
 
@@ -60,17 +47,6 @@ app.post(
   upload.single("image"),
   upload_controller.post_image_custom
 );
-
-// app.get("/images", async (req, res) => {
-//   const images = await controllers.get_image_category();
-//   if (images != null) {
-//     console.log(`success, images found: ${images}`);
-//     res.status(200).json(images);
-//   } else {
-//     console.log("error:cannot find images");
-//     res.sendStatus(400);
-//   }
-// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
