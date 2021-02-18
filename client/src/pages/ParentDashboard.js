55289f801feaab05ae3bb296bf43be0632102e5e
AgataPalka <agata.palka27@gmail.com>
2021-02-18 15:03:47 -0700

    AP - css/ParentDashboard; cleaning/ChangePassModal

--- client/src/pages/ParentDashboard.js
+++ client/src/pages/ParentDashboard.js
@@ -5,16 +5,15 @@ import ChangePasswordModal from "../components/ChangePasswordModal";
 import DeleteAccountModal from "../components/DeleteAccountModal";
 import { useHistory } from "react-router-dom";
 import { AuthContext } from "../components/AuthContext";
+import "../pages/ParentDashboard.css";
 
 const ParentDashboard = () => {
-  // const [categoryName, setCategoryName] = useState("");
-
   const {
     user,
     categoryName,
     setCategoryName,
     imagesUpload,
-    setImagesUpload,
+    // setImagesUpload,
   } = useContext(AuthContext);
 
   const handleSubmitClick = () => {
@@ -28,17 +27,49 @@ const ParentDashboard = () => {
   console.log("this is the imagesUpload state", user.imagesUpload);
 
   return (
-    <div>
-      <h2 className="parent-title">
-        Hello {user.username}! This is your Dashboard.{" "}
-      </h2>
-      <h4 className="dashboard-description">
-        You can upload your pictures or change your password.
-      </h4>
+    <div className="backgroundParentDashBoard">
+      <div className="parentDashboard">
+        <h2
+          className="parent-title"
+          style={{
+            marginTop: "2rem",
+            display: "flex",
+            justifyContent: "center",
+            fontFamily: "Henny Penny",
+            color: "purple",
+            marginLeft: "2rem",
+          }}
+        >
+          Hello {user.username}! This is your Parent Dashboard.{" "}
+        </h2>
+        {/* <h4
+          className="dashboard-description"
+          style={{
+            margin: "auto",
+            display: "flex",
+            justifyContent: "center",
+            // fontFamily: "Happy Monkey",
+          }}
+        >
+          You can upload your images or change your password.
+        </h4> */}
+      </div>
       <br />
       {/* this is the first card */}
-      <div className="card col-10">
-        <div className="card-header">Create Custom Category</div>
+      <div
+        className="card col-10"
+        style={{ margin: "auto", backgroundColor: "transparent" }}
+      >
+        <div
+          className="card-header"
+          style={{
+            fontFamily: "Happy Monkey",
+            fontSize: "20px",
+            fontWeight: "800",
+          }}
+        >
+          Create Custom Category
+        </div>
         <div className="card-body">
           <p className="card-text">
             Have fun with creating a custom category by uploading your own
@@ -67,8 +98,11 @@ const ParentDashboard = () => {
             type="button"
             className="btn btn-warning goPlay"
             onClick={handleSubmitClick}
+            style={{
+              marginLeft: 0,
+            }}
           >
-            Go play
+            Go Play
           </button>
         </div>
       </div>
@@ -76,8 +110,20 @@ const ParentDashboard = () => {
 
       {/* this is the second card */}
 
-      <div className="card col-10">
-        <div className="card-header">Change Password</div>
+      <div
+        className="card col-10"
+        style={{ margin: "auto", backgroundColor: "transparent" }}
+      >
+        <div
+          className="card-header"
+          style={{
+            fontFamily: "Happy Monkey",
+            fontSize: "20px",
+            fontWeight: "800",
+          }}
+        >
+          Change Password
+        </div>
         <div className="card-body">
           <p className="card-text">
             Think your password is compromised? We've got your back. Here you
@@ -88,9 +134,21 @@ const ParentDashboard = () => {
         </div>
       </div>
       <br />
-
-      <div className="card col-10">
-        <div className="card-header">Delete Account</div>
+      {/* this is the third card */}
+      <div
+        className="card col-10"
+        style={{ margin: "auto", backgroundColor: "transparent" }}
+      >
+        <div
+          className="card-header"
+          style={{
+            fontFamily: "Happy Monkey",
+            fontSize: "20px",
+            fontWeight: "800",
+          }}
+        >
+          Delete Account
+        </div>
         <div className="card-body">
           <p className="card-text">
             We will be sad to see you leave but we have an option for you to do
@@ -98,7 +156,6 @@ const ParentDashboard = () => {
           </p>
 
           <DeleteAccountModal user={user} />
-
         </div>
       </div>
 
